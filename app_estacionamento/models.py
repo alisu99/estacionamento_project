8495fc355696c967a35aaac9cf318af0d5407ba3
alisu99 <alissonsts910@gmail.com>
2023-12-06 22:11:26 -0300

    ...

--- app_estacionamento/models.py
+++ app_estacionamento/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Mensalista(models.Model):
     id = models.AutoField(primary_key=True)
     nome = models.CharField('Nome *', max_length=50)
@@ -8,7 +7,7 @@ class Mensalista(models.Model):
     valor = models.DecimalField('Valor *', max_digits=8, decimal_places=2)
     data_inicial = models.DateField('Data de início *')
     dia_vencimento = models.IntegerField('Dia de vencimetno *', null=True)
-    cep = models.IntegerField('CEP', null=True)
+    cep = models.CharField('CEP', max_length=9)
     endereco = models.CharField('Endereço', max_length=255, null=True)
     cidade = models.CharField('Cidade', max_length=255, null=True)
     uf = models.CharField('UF', max_length=2, null=True)
