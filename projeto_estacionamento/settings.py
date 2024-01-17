b28764a44e2ff62ad09046e19355bb2a6490e6de
alisu99 <alissonsts910@gmail.com>
2024-01-17 11:17:57 -0300

    migração para mysql

--- projeto_estacionamento/settings.py
+++ projeto_estacionamento/settings.py
@@ -46,7 +46,7 @@ ROOT_URLCONF = 'projeto_estacionamento.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,8 +67,12 @@ WSGI_APPLICATION = 'projeto_estacionamento.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "estacionamento_schema",
+        "USER": "root",
+        "PASSWORD": "132,mysql",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
