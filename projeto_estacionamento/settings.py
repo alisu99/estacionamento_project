7f3c4389809e9e9c799aeb5af3a0933e47860fd5
alisu99 <alissonsts910@gmail.com>
2024-01-18 11:27:16 -0300

    ...

--- projeto_estacionamento/settings.py
+++ projeto_estacionamento/settings.py
@@ -67,12 +67,8 @@ WSGI_APPLICATION = 'projeto_estacionamento.wsgi.application'
 
 DATABASES = {
     'default': {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "estacionamento_schema",
-        "USER": "root",
-        "PASSWORD": "132,postgresql",
-        "HOST": "localhost",
-        "PORT": "5432",
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
